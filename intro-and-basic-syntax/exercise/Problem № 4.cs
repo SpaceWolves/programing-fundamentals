a514974395cf636ed78e43dd3836ddfc6542389b
toster <kingtoster@gmail.com>
2018-03-10 10:50:12 +0200

    Changed energyContent to energyContentPer100ml and sugarContent to sugarContentPer100ml

--- intro-and-basic-syntax/exercise/Problem № 4.cs
+++ intro-and-basic-syntax/exercise/Problem № 4.cs
@@ -8,11 +8,11 @@ namespace Problem___4.cs
         {
             string productName = Console.ReadLine();
             int volumeInMililiters = int.Parse(Console.ReadLine());
-            int energyContent = int.Parse(Console.ReadLine());
-            int sugarContent = int.Parse(Console.ReadLine());
+            int energyContentPer100ml = int.Parse(Console.ReadLine());
+            int sugarContentPer100ml = int.Parse(Console.ReadLine());
 
-            double totalEnergy = (double)(volumeInMililiters * energyContent) / 100;
-            double totalSugar = (double)(volumeInMililiters * sugarContent) / 100;
+            double totalEnergy = (double)(volumeInMililiters * energyContentPer100ml) / 100;
+            double totalSugar = (double)(volumeInMililiters * sugarContentPer100ml) / 100;
 
             Console.WriteLine($"{volumeInMililiters}ml {productName}:");
             Console.WriteLine($"{totalEnergy}kcal, {totalSugar}g sugars");
