e85e07595aeba85efb5193f1e38f01a26db294b9
tanya staneva <tany130@abv.bg>
2018-03-10 15:08:02 +0200

    added new problem

---
+++ intro-and-basic-syntax/exercise/Problem № 3.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+
+class MultidimentionalArrays
+{
+    static void Main()
+    {
+        int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        int rows = size[0];
+        int cols = size[1];
+
+        string[,] result = new string[rows, cols];
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                char rowChar = alphabet[row];
+                char colChar = alphabet[col + row];
+                result[row, col] = string.Format($"{rowChar}{colChar}{rowChar}");
+            }
+        }
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Console.Write($"{result[row, col]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
