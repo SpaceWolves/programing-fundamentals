8a422b47ac48e594d5fd04736a8e245c4a12c211
antongeorgiev01 <anton.georgiev@abv.bg>
2018-03-15 12:46:26 +0200

    I documented the code.

--- intro-and-basic-syntax/exercise/Problem № 1.cs
+++ intro-and-basic-syntax/exercise/Problem № 1.cs
@@ -5,7 +5,9 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //In one line we receive four integers. 
+            //In the same line we print them.
+            //If the integers are with less than 4 digits, we add zeroes until they represent four digit numbers.
             Console.WriteLine($"{int.Parse(Console.ReadLine()):d4} {int.Parse(Console.ReadLine()):d4} {int.Parse(Console.ReadLine()):d4} {int.Parse(Console.ReadLine()):d4}");
         }
     }
