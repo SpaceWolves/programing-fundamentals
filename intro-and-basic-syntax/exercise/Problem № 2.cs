6dc6bb6a964a9506e280bfe2c2f4ca90cc596377
antongeorgiev01 <anton.georgiev@abv.bg>
2018-03-15 12:35:30 +0200

    This was an already pushed solution, I only renamed the file from Program №2 to Problem № 2.

---
+++ intro-and-basic-syntax/exercise/Problem № 2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());//Getting the width
+            var height = double.Parse(Console.ReadLine());//Getting the height
+            var area = (width * height);//Calculating the area
+            Console.WriteLine("{0:f2}", area);//Printing the area
+        }
+    }
+}
