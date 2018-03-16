40256a947eacba5e52385683343f763ae5c9d583
Avanguarde <koko.perko@gmail.com>
2018-03-16 13:08:07 +0200

    Commented!

--- intro-and-basic-syntax/exercise/Problem № 5.cs
+++ intro-and-basic-syntax/exercise/Problem № 5.cs
@@ -6,12 +6,14 @@ namespace Problem__5
     {
         static void Main(string[] args)
         {
+			// Reading the input from the console.
             string name = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
+			//Calculating and printing the result in a conditional format. 
             Console.WriteLine($"Name: {name}");
             Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}|");
             Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}|");
