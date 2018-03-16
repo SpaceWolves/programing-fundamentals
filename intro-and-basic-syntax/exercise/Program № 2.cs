bf2aa2d6fc1cc0eebb7b72e29d5460ecaa09cf97
Avanguarde <koko.perko@gmail.com>
2018-03-16 13:09:42 +0200

    Changed variable name on line 10.

--- intro-and-basic-syntax/exercise/Program № 2.cs
+++ intro-and-basic-syntax/exercise/Program № 2.cs
@@ -7,7 +7,7 @@ namespace Rectangle_Area
         static void Main(string[] args)
         {
             var width = double.Parse(Console.ReadLine());//Getting the width
-            var height = double.Parse(Console.ReadLine());//Getting the height
+            var heght = double.Parse(Console.ReadLine());//Getting the height
             var area = (width * height);//Calculating the area
             Console.WriteLine("{0:f2}", area);//Printing the area
         }
